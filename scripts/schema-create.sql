dab6794004c3d780d60497ffdb87eb720713900c
Pedro Henrique Medeiros Hermes <79016964+pedrohermes22@users.noreply.github.com>
2022-11-19 21:50:39 -0300

    Atualizando scripts de criação e inserção.

--- scripts/schema-create.sql
+++ scripts/schema-create.sql
@@ -19,12 +19,6 @@ CREATE TABLE subject (
     name VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE auth (
-    user_registration INT PRIMARY KEY,
-    login VARCHAR(64) NOT NULL,
-    password VARCHAR(40) NOT NULL
-);
-
 CREATE TABLE center (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(64) UNIQUE NOT NULL,
@@ -39,6 +33,23 @@ CREATE TABLE department (
     CONSTRAINT fk_department_center_id FOREIGN KEY (center_id) REFERENCES center (id)
 );
 
+CREATE TABLE user (
+    registration INT PRIMARY KEY,
+    first_name VARCHAR(64) NOT NULL,
+    last_name VARCHAR(64) NOT NULL,
+    role_id INT NOT NULL,
+    department_id INT NOT NULL,
+    CONSTRAINT fk_user_role_id FOREIGN KEY (role_id) REFERENCES role (id),
+    CONSTRAINT fk_user_department_id FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+CREATE TABLE auth (
+    user_registration INT PRIMARY KEY,
+    login VARCHAR(64) NOT NULL,
+    password VARCHAR(40) NOT NULL,
+    CONSTRAINT fk_auth_user_registration FOREIGN KEY (user_registration) REFERENCES user (registration)
+);
+
 CREATE TABLE class (
     id VARCHAR(16) PRIMARY KEY,
     type INT NOT NULL,
@@ -50,22 +61,12 @@ CREATE TABLE class (
     CONSTRAINT fk_class_department_id FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
-CREATE TABLE user (
-    registration INT PRIMARY KEY,
-    first_name VARCHAR(64) NOT NULL,
-    last_name VARCHAR(64) NOT NULL,
-    role_id INT NOT NULL,
-    department_id INT NOT NULL,
-    CONSTRAINT fk_user_role_id FOREIGN KEY (role_id) REFERENCES role (id),
-    CONSTRAINT fk_user_department_id FOREIGN KEY (department_id) REFERENCES department (id)
-);
-
 -- Informa as matérias às quais o estudante está matriculado.
 -- start_date: o início do semestre.
 -- end_date: o fim do semestre.
 CREATE TABLE subject_history (
-    start_date DATETIME NOT NULL,
-    end_date DATETIME NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
     subject_id VARCHAR(16) NOT NULL,
     student_id INT NOT NULL,
     teacher_id INT NOT NULL,
--- scripts/schema-populate.sql
+++ scripts/schema-populate.sql
@@ -8,6 +8,11 @@ VALUES
     ('Coordenação');
 
 INSERT
+    IGNORE INTO desk_type (name)
+VALUES
+    ('Comum');
+
+INSERT
     IGNORE INTO subject (id, name)
 VALUES
     ('1MAT177', 'Cálculo 1'),
@@ -17,15 +22,117 @@ VALUES
     ('1COP029', 'Compiladores 2');
 
 INSERT
-    IGNORE INTO auth (user_registration, login, password)
-VALUES
-    ('202000560227', 'Hermes', '1'),
-    ('1', 'Jennifer', '2'),
-    ('2', 'Isabela', '3'),
-    ('3', 'Jandira G Palma', '4');
-
-INSERT
     IGNORE INTO center (name, acronym)
 VALUES
     ('Centro de Ciências Exatas', 'CCE'),
     ('Centro de Ciências Agrárias', 'CCA');
+
+INSERT
+    IGNORE INTO department (name, acronym, center_id)
+VALUES
+    ('Departamento de Computação', 'DC', 1);
+
+INSERT
+    IGNORE INTO user (
+        registration,
+        first_name,
+        last_name,
+        role_id,
+        department_id
+    )
+VALUES
+    (
+        '20201',
+        'Pedro Henrique',
+        'Medeiros Hermes',
+        1,
+        1
+    ),
+    ('20202', 'Jennifer', 'do Prado da Silva', 1, 1),
+    ('20203', 'Isabela', 'Hara Bando', 1, 1),
+    (
+        '20204',
+        'Pedro Eduardo',
+        'Garbossa de Almeida',
+        1,
+        1
+    ),
+    ('20205', 'Denise', 'Rezende', 1, 1),
+    (
+        '20206',
+        'Israel',
+        'Faustino Botelho Junior',
+        1,
+        1
+    ),
+    ('20207', 'Jandira', 'Guenka Palma', 2, 1),
+    ('20208', 'Wesley', 'Attrot', 2, 1);
+
+INSERT
+    IGNORE INTO auth (user_registration, login, password)
+VALUES
+    ('20201', 'Hermes', '1'),
+    ('20202', 'Jennifer', '1'),
+    ('20203', 'Isabela', '1'),
+    ('20204', 'Eduardo', '1'),
+    ('20205', 'Denise', '1'),
+    ('20206', 'Israel', '1'),
+    ('20207', 'Jandira', '1');
+
+INSERT
+    IGNORE INTO class (
+        id,
+        type,
+        capacity,
+        acessible,
+        desk_type_id,
+        department_id
+    )
+VALUES
+    ('MDC-1', 0, 20, FALSE, 1, 1),
+    ('MDC-2', 0, 20, FALSE, 1, 1);
+
+INSERT
+    IGNORE INTO subject_history (
+        start_date,
+        end_date,
+        subject_id,
+        student_id,
+        teacher_id
+    )
+VALUES
+    (
+        '2022-01-23',
+        '2022-06-30',
+        '1COP022',
+        20201,
+        20207
+    );
+
+INSERT
+    IGNORE INTO class_reservation (requester_id, class_id, start_date, end_date)
+VALUES
+    (
+        20207,
+        'MDC-1',
+        '2022-11-01 08:20:00',
+        '2022-11-01 10:00:00'
+    ),
+    (
+        20207,
+        'MDC-2',
+        '2022-11-01 10:15:00',
+        '2022-11-01 11:55:00'
+    ),
+    (
+        20208,
+        'MDC-2',
+        '2022-11-03 08:20:00',
+        '2022-11-03 11:55:00'
+    ),
+    (
+        20207,
+        'MDC-2',
+        '2022-11-04 14:00:00',
+        '2022-11-05 17:35:00'
+    );
